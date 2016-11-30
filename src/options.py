784f5291e5794b38306b52c081632e06b78882f6
Alex Siryi <desertkun@gmail.com>
2016-11-30 17:20:51 +0200

    Refused the *.anthill.local domain for local testing, using *.anthill instead

--- src/options.py
+++ src/options.py
@@ -4,7 +4,7 @@ from common.options import define
 # Main
 
 define("host",
-       default="http://login-dev.anthill.local",
+       default="http://login-dev.anthill",
        help="Public hostname of this service",
        type=str)
 
