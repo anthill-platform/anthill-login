bb71a7fd4845d64f10cac8fe55d8af1cdf0dbc4f
Alex Siryi <desertkun@gmail.com>
2017-10-28 17:56:10 +0300

    Updated default listen option

--- src/options.py
+++ src/options.py
@@ -9,7 +9,7 @@ define("host",
        type=str)
 
 define("listen",
-       default="unix:/usr/local/var/run/anthill/dev_login.sock",
+       default="port:9501",
        help="Socket to listen. Could be a port number (port:N), or a unix domain socket (unix:PATH)",
        type=str)
 
